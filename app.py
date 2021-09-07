9935fb08950b205b2c8795657b7f579e6ce76582
RobMilinski <88419926+RobMilinski@users.noreply.github.com>
2021-09-07 18:35:47 +1000

    Add files via upload
    Initial upload to own branch. Changes made, custom invoice search working

---
+++ app.py
@@ -0,0 +1,210 @@
+# -*- coding: utf-8 -*-
+import os
+from functools import wraps
+from io import BytesIO
+from logging.config import dictConfig
+
+from flask import Flask, url_for, render_template, session, redirect, json, send_file
+from flask_oauthlib.contrib.client import OAuth, OAuth2Application
+from flask_session import Session
+from xero_python.accounting import AccountingApi, ContactPerson, Contact, Contacts
+from xero_python.api_client import ApiClient, serialize
+from xero_python.api_client.configuration import Configuration
+from xero_python.api_client.oauth2 import OAuth2Token
+from xero_python.exceptions import AccountingBadRequestException
+from xero_python.identity import IdentityApi
+from xero_python.utils import getvalue
+
+import logging_settings
+from utils import jsonify, serialize_model
+
+from flask import request
+import json
+
+dictConfig(logging_settings.default_settings)
+
+# configure main flask application
+app = Flask(__name__)
+app.config.from_object("default_settings")
+app.config.from_pyfile("config.py", silent=True)
+
+if app.config["ENV"] != "production":
+    # allow oauth2 loop to run over http (used for local testing only)
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+# configure persistent session cache
+Session(app)
+
+# configure flask-oauthlib application
+# TODO fetch config from https://identity.xero.com/.well-known/openid-configuration #1
+oauth = OAuth(app)
+xero = oauth.remote_app(
+    name="xero",
+    version="2",
+    client_id=app.config["CLIENT_ID"],
+    client_secret=app.config["CLIENT_SECRET"],
+    endpoint_url="https://api.xero.com/",
+    authorization_url="https://login.xero.com/identity/connect/authorize",
+    access_token_url="https://identity.xero.com/connect/token",
+    refresh_token_url="https://identity.xero.com/connect/token",
+    scope="offline_access openid profile email accounting.transactions "
+    "accounting.reports.read accounting.journals.read accounting.settings "
+    "accounting.contacts accounting.attachments assets projects",
+)  # type: OAuth2Application
+
+
+# configure xero-python sdk client
+api_client = ApiClient(
+    Configuration(
+        debug=app.config["DEBUG"],
+        oauth2_token=OAuth2Token(
+            client_id=app.config["CLIENT_ID"], client_secret=app.config["CLIENT_SECRET"]
+        ),
+    ),
+    pool_threads=1,
+)
+
+
+# configure token persistence and exchange point between flask-oauthlib and xero-python
+@xero.tokengetter
+@api_client.oauth2_token_getter
+def obtain_xero_oauth2_token():
+    return session.get("token")
+
+
+@xero.tokensaver
+@api_client.oauth2_token_saver
+def store_xero_oauth2_token(token):
+    session["token"] = token
+    session.modified = True
+
+
+def xero_token_required(function):
+    @wraps(function)
+    def decorator(*args, **kwargs):
+        xero_token = obtain_xero_oauth2_token()
+        if not xero_token:
+            return redirect(url_for("login", _external=True))
+
+        return function(*args, **kwargs)
+
+    return decorator
+
+
+@app.route("/")
+def index():
+    xero_access = dict(obtain_xero_oauth2_token() or {})
+    return render_template(
+        "code.html",
+        title="Home | oauth token",
+        code=json.dumps(xero_access, sort_keys=True, indent=4),
+    )
+
+@app.route("/test", methods=['GET', 'POST'])
+@xero_token_required
+def test():
+    xero_access = dict(obtain_xero_oauth2_token() or {})
+
+    if request.method == 'POST':
+        #when educator inputs invoice ID
+        xero_tenant_id = get_xero_tenant_id()
+        accounting_api = AccountingApi(api_client)
+
+        invoice_id = request.form['input_invoice_id']
+
+        invoice = accounting_api.get_invoice(
+            xero_tenant_id, 
+            invoice_id
+        )
+        code = serialize_model(invoice)
+        sub_title = "Invoice Requested"
+
+        return render_template(
+            "code.html", title="Custom Invoice", code=code, sub_title=sub_title
+        )
+    #if page called from navbar, initial open
+    return render_template("test.html", title="Home | GET Page")
+
+@app.route("/invoices")
+@xero_token_required
+def get_invoices():
+    xero_tenant_id = get_xero_tenant_id()
+    accounting_api = AccountingApi(api_client)
+
+    invoices = accounting_api.get_invoices(
+        xero_tenant_id, statuses=["DRAFT", "SUBMITTED"]
+    )
+    code = serialize_model(invoices)
+    sub_title = "Total invoices found: {}".format(len(invoices.invoices))
+
+    return render_template(
+        "code.html", title="Invoices", code=code, sub_title=sub_title
+    )
+
+@app.route("/login")
+def login():
+    redirect_url = url_for("oauth_callback", _external=True)
+    response = xero.authorize(callback_uri=redirect_url)
+    return response
+
+
+@app.route("/callback")
+def oauth_callback():
+    try:
+        response = xero.authorized_response()
+    except Exception as e:
+        print(e)
+        raise
+    # todo validate state value
+    if response is None or response.get("access_token") is None:
+        return "Access denied: response=%s" % response
+    store_xero_oauth2_token(response)
+    return redirect(url_for("index", _external=True))
+
+
+@app.route("/logout")
+def logout():
+    store_xero_oauth2_token(None)
+    return redirect(url_for("index", _external=True))
+
+
+@app.route("/export-token")
+@xero_token_required
+def export_token():
+    token = obtain_xero_oauth2_token()
+    buffer = BytesIO("token={!r}".format(token).encode("utf-8"))
+    buffer.seek(0)
+    return send_file(
+        buffer,
+        mimetype="x.python",
+        as_attachment=True,
+        attachment_filename="oauth2_token.py",
+    )
+
+
+@app.route("/refresh-token")
+@xero_token_required
+def refresh_token():
+    xero_token = obtain_xero_oauth2_token()
+    new_token = api_client.refresh_oauth2_token()
+    return render_template(
+        "code.html",
+        title="Xero OAuth2 token",
+        code=jsonify({"Old Token": xero_token, "New token": new_token}),
+        sub_title="token refreshed",
+    )
+
+
+def get_xero_tenant_id():
+    token = obtain_xero_oauth2_token()
+    if not token:
+        return None
+
+    identity_api = IdentityApi(api_client)
+    for connection in identity_api.get_connections():
+        if connection.tenant_type == "ORGANISATION":
+            return connection.tenant_id
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
